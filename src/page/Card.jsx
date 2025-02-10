f31cb918378ad6c233acc6893c4e7737a4c7a729
grgsudhiv123 <grgsudeep667@gmail.com>
2025-02-10 05:23:11 -0800

    few changes

--- src/page/Card.jsx
+++ src/page/Card.jsx
@@ -1,5 +1,7 @@
 // import img from "../assets/assets/images/illustration-empty.svg"
 import pound from "../assets/assets/images/pound-sign-svgrepo-com.svg"
+import calculator from "../assets/assets/images/icon-calculator.svg"
+
 const Card = () => {
   return (
     <section className="mx-auto 2xl:min-w-[1240px] xl:min-w-[1024px] bg-white rounded-3xl ">
@@ -16,7 +18,7 @@ const Card = () => {
                         <div className="w-[10%] flex justify-center bg-sky-200 h-full rounded-l-[5px]">
                             <img src={pound} alt="pound-image" width={20} height={20} className="object-contain bg-cover"/>
                         </div>
-                        <input type="text" required className="w-[90%] px-5 py-1 outline-none rounded-r-md my-0 mx-0"/>
+                        <input type="text" required className="w-[90%] px-2 py-1 outline-none rounded-r-md my-0 mx-0"/>
                     </div>
                 </div>
 
@@ -26,32 +28,40 @@ const Card = () => {
                     <div className="flex flex-col w-[45%]">
                     <p className="">Mortgage Term</p>
                     <div className="flex flex-row justify-start items-center rounded-md mt-2 border border-blue-900">
-                        <input type="text" required className="w-[70%] px-5 py-1 outline-none rounded-r-md my-0 mx-0"/>
-                        <div className="w-[30%] flex justify-center items-center bg-sky-200 h-full rounded-l-[5px]">
-                            <p>Years</p>
+                        <input type="text" required className="w-[70%] px-2 py-1 outline-none rounded-l-md my-0 mx-0"/>
+                        <div className="w-[30%] flex justify-center items-center bg-sky-200 h-full rounded-r-[5px]">
+                            <p>years</p>
                         </div>
                     </div>    
                     </div>
                     <div className="flex flex-col w-[45%]">
-                    <p className="">Interest Rate</p>
-                    <div className="bg-slate-500 flex flex-row justify-start items-center p-[2px] rounded-md mt-2">
-                        <input type="text" required className="w-[80%] px-5 py-1 outline-none rounded-l-[3px]"/>
-                        <p className="w-[20%] text-center">%</p>
-                    </div>    
+                        <p className="">Interest Rate</p>
+                        <div className="flex flex-row justify-start items-center rounded-md border border-blue-900 mt-2">
+                            <input type="text" required className="w-[80%] px-2 py-1 outline-none rounded-l-md my-0 mx-0"/>
+                            <div className="w-[20%] flex justify-center items-center bg-sky-200 h-full rounded-r-[5px]">
+                            <p className="text-center">%</p>
+                            </div>
+                        </div>    
                     </div>
                 </div>
 
 
                 <div className="flex flex-col mt-5">
                     <p>Mortgage Type</p>
-                    <div className="w-full flex flex-row h-10 justify-start items-center gap-5 p-3 border-[1px] border-slate-500 mt-2 rounded-md">
+                    <div className="w-full flex flex-row h-10 justify-start items-center gap-5 p-3 border border-blue-900 mt-2 rounded-md">
                         <input type="radio" name="repayment" value="repayment" /> <p>Repayment</p>
                     </div>
-                    <div className="w-full flex flex-row h-10 justify-start items-center gap-5 p-3 border-[1px] border-slate-500 mt-2 rounded-md">
+                    <div className="w-full flex flex-row h-10 justify-start items-center gap-5 p-3 border border-blue-900 mt-2 rounded-md">
                         <input type="radio" name="interest" value="interest" /> <p>Interest Only</p>
                     </div>
-                    
                 </div>
+
+
+                <div className="w-[70%] flex bg-lime-400 p-2 rounded-full gap-2 mt-10 justify-center items-center">
+                    <img src={calculator} alt="calculator" width={16} height={16}/>
+                    <p>Calculate Repayments</p>
+                </div>
+
 
             </div>
             <div className="flex flex-1 justify-center bg-teal-900 rounded-r-3xl rounded-bl-[80px] p-10">
